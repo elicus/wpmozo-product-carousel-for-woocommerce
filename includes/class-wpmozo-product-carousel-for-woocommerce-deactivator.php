f3208eebc1ff06603db0fd99c9f3afb499f0db2f
himanshudhakan <himanshudhakan9@gmail.com>
2023-05-25 11:22:53 +0530

    Base plugin

---
+++ includes/class-wpmozo-product-carousel-for-woocommerce-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://elicus.com
+ * @since      1.0.0
+ *
+ * @package    Wpmozo_Product_Carousel_For_Woocommerce
+ * @subpackage Wpmozo_Product_Carousel_For_Woocommerce/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wpmozo_Product_Carousel_For_Woocommerce
+ * @subpackage Wpmozo_Product_Carousel_For_Woocommerce/includes
+ * @author     Elicus <hello@elicus.com>
+ */
+class Wpmozo_Product_Carousel_For_Woocommerce_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ includes/class-wpmozo-product-carousel-for-woocommerce-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://elicus.com
+ * @since      1.0.0
+ *
+ * @package    Wpmozo_Product_Carousel_For_Woocommerce
+ * @subpackage Wpmozo_Product_Carousel_For_Woocommerce/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Wpmozo_Product_Carousel_For_Woocommerce
+ * @subpackage Wpmozo_Product_Carousel_For_Woocommerce/includes
+ * @author     Elicus <hello@elicus.com>
+ */
+class Wpmozo_Product_Carousel_For_Woocommerce_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'wpmozo-product-carousel-for-woocommerce',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}

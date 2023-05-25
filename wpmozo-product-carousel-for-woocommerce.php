3238afa75307c792f1a4e54bfde360d2cdb56ff8
himanshudhakan <himanshudhakan9@gmail.com>
2023-05-25 12:02:10 +0530

    Fix - removed activation and deactivation hook from main file.

--- wpmozo-product-carousel-for-woocommerce.php
+++ wpmozo-product-carousel-for-woocommerce.php
@@ -42,28 +42,6 @@ define( 'WPMOZO_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
 // Includes dir path.
 define( 'WPMOZO_INC_DIR_PATH', WPMOZO_PLUGIN_DIR_PATH . 'includes/' );
 
-
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-wpmozo-product-carousel-for-woocommerce-activator.php
- */
-function activate_wpmozo_product_carousel_for_woocommerce() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wpmozo-product-carousel-for-woocommerce-activator.php';
-	Wpmozo_Product_Carousel_For_Woocommerce_Activator::activate();
-}
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-wpmozo-product-carousel-for-woocommerce-deactivator.php
- */
-function deactivate_wpmozo_product_carousel_for_woocommerce() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wpmozo-product-carousel-for-woocommerce-deactivator.php';
-	Wpmozo_Product_Carousel_For_Woocommerce_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_wpmozo_product_carousel_for_woocommerce' );
-register_deactivation_hook( __FILE__, 'deactivate_wpmozo_product_carousel_for_woocommerce' );
-
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
